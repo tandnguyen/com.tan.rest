6180e84ea96119b8f1d23a44c28d9e4d660cb2b5
tandnguyen <tan.nguyen@pega.com>
2015-01-10 10:45:35 -0500

    Post working

--- src/com/tan/dao/SchemaOracleEmployee.java
+++ src/com/tan/dao/SchemaOracleEmployee.java
@@ -4,8 +4,54 @@ import org.codehaus.jettison.json.JSONArray;
 import com.tan.util.ToJSON;
 
 public class SchemaOracleEmployee extends OracleEmployee {
+	
+	public int insertIntoEMP_TABLE( 
+			String FIRST_NAME, 
+			String LAST_NAME,
+			String DEPT,
+			String TITLE)
+			throws Exception {
+		
+		PreparedStatement query = null;
+		Connection conn = null;
+		
+		try {
+			/*
+			 * If this was a real application, you should do data validation here
+			 * before starting to insert data into the database.
+			 * 
+			 * Important: The primary key on PC_PARTS table will auto increment.
+			 * 		That means the PC_PARTS_PK column does not need to be apart of the 
+			 * 		SQL insert query below.
+			 */
+			conn = oracleEmployeesConnection();
+			query = conn.prepareStatement("insert into EMP_TABLE1 " +
+					"(FIRST_NAME, LAST_NAME, DEPT, TITLE) " +
+					"VALUES ( ?, ?, ?, ? ) ");
+
+			query.setString(1, FIRST_NAME);
+			query.setString(2, LAST_NAME);
+			query.setString(3, DEPT);
+			query.setString(4, TITLE);
+
+			System.out.println(query);
+			query.executeUpdate(); //note the new command for insert statement
+
+		} catch(Exception e) {
+			e.printStackTrace();
+			return 500; //if a error occurs, return a 500
+		}
+		finally {
+			if (conn != null) conn.close();
+		}
+		
+		return 200;
+	}
+	
 
 	public JSONArray queryReturnEmployeesDept(String department) throws Exception {
+		
+
 		
 		PreparedStatement query = null;
 		Connection conn = null;
@@ -15,7 +61,7 @@ public class SchemaOracleEmployee extends OracleEmployee {
 		
 		try {
 			conn =  oracleEmployeesConnection();
-			query = conn.prepareStatement("select EMP_NUMBER, FIRST_NAME, LAST_NAME, DEPT, TITLE, HIRE_DATE from EMP_TABLE where UPPER(DEPT) = ? ");
+			query = conn.prepareStatement("select FIRST_NAME, LAST_NAME, DEPT, TITLE from EMP_TABLE1 where UPPER(DEPT) = ? ");
 			
 			query.setString(1, department.toUpperCase()); //protect against sql injection
 			ResultSet rs = query.executeQuery();
--- src/com/tan/rest/employee/V2_Employees.java
+++ src/com/tan/rest/employee/V2_Employees.java
@@ -1,5 +1,7 @@
 package com.tan.rest.employee;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.GET;
 import javax.ws.rs.Produces;
@@ -7,7 +9,9 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jettison.json.JSONArray;
+
 import com.tan.dao.SchemaOracleEmployee;
 
 @Path("/v2/employees")
@@ -34,5 +38,55 @@ public class V2_Employees {
 		}
 		return Response.ok(returnString).build();
 	}
+	
+	@POST
+	@Consumes({MediaType.APPLICATION_FORM_URLENCODED,MediaType.APPLICATION_JSON})
+	//@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response addPcParts(String incomingData) throws Exception {
+		
+		String returnString = null;
+		JSONArray jsonArray = new JSONArray(); 
+		SchemaOracleEmployee dao = new SchemaOracleEmployee();
+		
+		try {
+			System.out.println("incomingData: " + incomingData);
+			
+			/*
+			 * ObjectMapper is from Jackson Processor framework
+			 * http://jackson.codehaus.org/
+			 * 
+			 * Using the readValue method, you can parse the json from the http request
+			 * and data bind it to a Java Class.
+			 */
+			ObjectMapper mapper = new ObjectMapper();  
+			ItemEntry itemEntry = mapper.readValue(incomingData, ItemEntry.class);
+			
+			int http_code = dao.insertIntoEMP_TABLE(itemEntry.FIRST_NAME, 
+													itemEntry.LAST_NAME, 
+													itemEntry.DEPT, 
+													itemEntry.TITLE );
+			
+			if( http_code == 200 ) {
+				//returnString = jsonArray.toString();
+				returnString = "Item inserted";
+			} else {
+				return Response.status(500).entity("Unable to process Item").build();
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			return Response.status(500).entity("Server was not able to process your request").build();
+		}
+		
+		return Response.ok(returnString).build();
+	}
+}
 
+
+class ItemEntry {
+	public String FIRST_NAME ;
+	public String LAST_NAME ;
+	public String DEPT;
+	public String TITLE;
 }
